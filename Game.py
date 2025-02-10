a7e0d55bf1c4a79c368b3f03c8b87b68ff08c7ee
Pollo963 <paul.alexandre0706@gmail.com>
2025-02-10 09:05:57 +0100

    M

---
+++ Game.py
@@ -0,0 +1,37 @@
+import pygame
+import pytmx
+import pyscroll
+
+from player import Player
+
+
+
+
+class Game:
+    def __init__(self):
+
+        self.screen = pygame.display.set_mode((800, 800))
+        pygame.display.set_caption("Pygame")
+
+        tmx_data = pytmx.util_pygame.load_pygame('pokemonn.tmx')
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        map_layer.zoom=1
+
+        player_postion=tmx_data.get_object_by_name("Player")
+        self.player=Player(player_postion.x, player_postion.y)
+
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=3)
+        self.group.add(self.player)
+
+    def run(self):
+        running = True
+        while running:
+            self.group.update()
+            self.group.center(self.player.rect)
+            self.group.draw(self.screen)
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import pygame
+pygame.init()
+
+from Game import Game
+
+if __name__ == '__main__':
+    pygame.init()
+    Game=Game()
+    Game.run()
+
+
+pygame.quit()
