608b406ef788132a4c6d707cbfed633dc6a45535
justachillguy <lucas.singhavara@efrei.net>
2025-03-03 11:12:57 +0100

    walls

--- Game.py
+++ Game.py
@@ -16,10 +16,16 @@ class Game:
         tmx_data = pytmx.util_pygame.load_pygame('Tileset/pokemonn.tmx')
         map_data = pyscroll.data.TiledMapData(tmx_data)
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
-        map_layer.zoom=1
+        map_layer.zoom=2
 
         player_postion=tmx_data.get_object_by_name("Player")
         self.player=Player(player_postion.x, player_postion.y)
+
+        self.walls = []
+
+        for obj in tmx_data.objects:
+            if obj.type == "col":
+                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
 
         self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=3)
         self.group.add(self.player)
@@ -29,21 +35,30 @@ class Game:
 
         if pressed[pygame.K_UP]:
             self.player.move_up()
+            self.player.change_animation('up')
         elif pressed[pygame.K_DOWN]:
             self.player.move_down()
+            self.player.change_animation('down')
         elif pressed[pygame.K_RIGHT]:
             self.player.move_right()
+            self.player.change_animation('right')
         elif pressed[pygame.K_LEFT]:
            self.player.move_left()
-
+           self.player.change_animation('left')
+    def update(self):
+        self.group.update()
+        for sprite in self.group.sprites():
+            if sprite.feet.colliderect(self.walls)>-1:
+                sprite.move_back()
     def run(self):
         clock = pygame.time.Clock()
 
         running = True
 
         while running:
+            self.player.save_location()
             self.handle_input()
-            self.group.update()
+            self.update()
             self.group.center(self.player.rect)
             self.group.draw(self.screen)
             pygame.display.flip()
