849518a4663defb58a8aaf0d54267c1f23f11421
justachillguy <lucas.singhavara@efrei.net>
2025-03-03 22:45:08 +0100

    walls

--- Game.py
+++ Game.py
@@ -1,71 +1,62 @@
 import pygame
 import pytmx
 import pyscroll
-
 from player import Player
-
-
-
 
 class Game:
     def __init__(self):
-
         self.screen = pygame.display.set_mode((800, 800))
         pygame.display.set_caption("Pygame")
 
         tmx_data = pytmx.util_pygame.load_pygame('Tileset/horror.tmx')
         map_data = pyscroll.data.TiledMapData(tmx_data)
-        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
-        map_layer.zoom=2
+        self.map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        self.map_layer.zoom = 2
 
-        player_postion=tmx_data.get_object_by_name("Player")
-        self.player=Player(player_postion.x, player_postion.y)
+        player_position = tmx_data.get_object_by_name("Player")
+        self.player = Player(player_position.x, player_position.y)
 
         self.walls = []
-
         for obj in tmx_data.objects:
             if obj.type == "collision":
                 self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
 
-        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=4)
+        self.group = pyscroll.PyscrollGroup(map_layer=self.map_layer, default_layer=4)
         self.group.add(self.player)
 
     def handle_input(self):
         pressed = pygame.key.get_pressed()
 
+        self.player.save_location()
+
         if pressed[pygame.K_UP]:
             self.player.move_up()
-            self.player.change_animation('up')
         elif pressed[pygame.K_DOWN]:
             self.player.move_down()
-            self.player.change_animation('down')
         elif pressed[pygame.K_RIGHT]:
             self.player.move_right()
-            self.player.change_animation('right')
         elif pressed[pygame.K_LEFT]:
-           self.player.move_left()
-           self.player.change_animation('left')
+            self.player.move_left()
 
     def update(self):
-        self.group.update()
+        self.player.update()
 
-        for sprite in self.group.sprites():
-            if sprite.feet.collidelist(self.walls) > -1:
-                sprite.move_back()
+        if self.player.feet.collidelist(self.walls) > -1:
+            self.player.move_back()
+
+        self.group.center(self.player.rect.center)
 
     def run(self):
         clock = pygame.time.Clock()
-        for wall in self.walls:
-            pygame.draw.rect(self.screen, (255, 0, 0), wall, 2)
-
         running = True
 
         while running:
-            self.player.save_location()
             self.handle_input()
             self.update()
-            self.group.center(self.player.rect)
+
+            self.screen.fill((0, 0, 0))
             self.group.draw(self.screen)
+
             pygame.display.flip()
 
             for event in pygame.event.get():
@@ -73,3 +64,4 @@ class Game:
                     running = False
 
             clock.tick(60)
+
--- player.py
+++ player.py
@@ -1,47 +1,64 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         super().__init__()
-        self.sprite_sheet = pygame.image.load('Tileset/Player.png')
+        self.sprite_sheet = pygame.image.load('Tileset/H5TPB4.png').convert_alpha()
         self.image = self.get_image(0, 0)
-        self.image.set_colorkey([0, 0, 0])
-        self.rect = self.image.get_rect()
-        self.position=[x,y]
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.position = [x, y]
+
         self.images = {
-            'down':self.get_image(0, 0),
-            'left':self.get_image(0, 32),
-            'right':self.get_image(0, 64),
-            'up':self.get_image(0, 96),
+            'down': [self.get_image(0, 0), self.get_image(1, 0), self.get_image(2, 0)],
+            'left': [self.get_image(0, 1), self.get_image(1, 1), self.get_image(2, 1)],
+            'right': [self.get_image(0, 2), self.get_image(1, 2), self.get_image(2, 2)],
+            'up': [self.get_image(0, 3), self.get_image(1, 3), self.get_image(2, 3)],
         }
+
+        self.current_frame = 0
+        self.animation_speed = 15
+        self.animation_counter = 0
+        self.direction = 'down'
+
         self.feet = pygame.Rect(0, 0, self.rect.width * 0.5, 15)
         self.old_position = self.position.copy()
         self.speed = 2
 
-    def save_location(self):self.old_position = self.position.copy()
+    def save_location(self):
+        self.old_position = self.position.copy()
 
-    def change_animation(self,name):
-        self.image = self.images[name]
-        self.image.set_colorkey([0, 0, 0])
+    def change_animation(self, direction):
+        self.direction = direction
+        self.animation_counter += 1
+
+        if self.animation_counter >= self.animation_speed:
+            self.current_frame = (self.current_frame + 1) % len(self.images[direction])
+            self.animation_counter = 0
+
+        self.image = self.images[direction][self.current_frame]
 
     def move_right(self):
         self.position[0] += self.speed
+        self.change_animation("right")
         self.rect.topleft = self.position
 
     def move_left(self):
         self.position[0] -= self.speed
+        self.change_animation("left")
         self.rect.topleft = self.position
 
     def move_up(self):
         self.position[1] -= self.speed
+        self.change_animation("up")
         self.rect.topleft = self.position
 
     def move_down(self):
         self.position[1] += self.speed
+        self.change_animation("down")
         self.rect.topleft = self.position
 
     def update(self):
-        self.rect.topleft=self.position
+        self.rect.topleft = self.position
         self.feet.midbottom = self.rect.midbottom
 
     def move_back(self):
@@ -49,7 +66,10 @@ class Player(pygame.sprite.Sprite):
         self.rect.topleft = self.position
         self.feet.midbottom = self.rect.midbottom
 
-    def get_image(self, x, y):
-        image = pygame.Surface([32, 32], pygame.SRCALPHA)
-        image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 32))
+    def get_image(self, col, row):
+        sprite_width, sprite_height = 32, 48
+        x = col * sprite_width
+        y = row * sprite_height
+        image = pygame.Surface([sprite_width, sprite_height], pygame.SRCALPHA)
+        image.blit(self.sprite_sheet, (0, 0), (x, y, sprite_width, sprite_height))
         return image.convert_alpha()
