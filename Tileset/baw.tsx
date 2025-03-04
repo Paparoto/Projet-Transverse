29804a375a16463bbb73a49ccc9114125206e1a4
Pollo963 <paul.alexandre0706@gmail.com>
2025-03-04 20:26:25 +0100

    Add files via upload

---
+++ Tileset/baw.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="baw" tilewidth="16" tileheight="16" tilecount="520" columns="20">
+ <image source="tileset_32x32_1bit.png" trans="000000" width="320" height="416"/>
+</tileset>
