4240d737a0e3fb13ec86983b251ee515fbe56217
Pollo963 <paul.alexandre0706@gmail.com>
2025-03-17 15:29:41 +0100

    Add files via upload

---
+++ Tileset/16x161bit.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="16x161bit" tilewidth="16" tileheight="16" tilecount="60" columns="10">
+ <image source="16x161bit.png" width="164" height="96"/>
+</tileset>
---
+++ Tileset/tree3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tree3" tilewidth="16" tileheight="16" tilecount="10" columns="5">
+ <image source="tree3.bmp" width="81" height="33"/>
+</tileset>
